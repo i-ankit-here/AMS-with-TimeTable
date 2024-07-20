3c7601647391510bd25230b5c952d57fda17744c
ANKIT <closetooankit@gmail.com>
2024-07-20 21:56:26 +0530

    Update getApiURL.js

--- server/src/modules/certificateModule/helper/getApiURL.js
+++ server/src/modules/certificateModule/helper/getApiURL.js
@@ -3,11 +3,11 @@ function getApiURL(url) {
     const currentURL = url;
     const localhostBackend = 'http://localhost:8010';
     const nitjServer = 'https://xceed.nitj.ac.in';
-    const productionFrontend = 'https://nitjtt.onrender.com';
+    const productionFrontend = 'https://ams-with-timetable.onrender.com';
   
     if (currentURL.includes('localhost')) {
       return localhostBackend;
-    } else if (currentURL.includes('nitjtt')) {
+    } else if (currentURL.includes('ams')) {
       return productionFrontend;
     } else if (currentURL.includes('xceed.nitj.ac.in')) {
       return nitjServer;
