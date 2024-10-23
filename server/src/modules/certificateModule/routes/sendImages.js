7f4a3fac954b85d50d95113203f01c5f3dc66ae3
ANKIT <closetooankit@gmail.com>
2024-10-23 21:56:00 +0530

    Update sendImages.js

--- server/src/modules/certificateModule/routes/sendImages.js
+++ server/src/modules/certificateModule/routes/sendImages.js
@@ -2,14 +2,10 @@ const express = require("express");
 const imageRouter = express.Router();
 const path = require("path")
 
-imageRouter.get("/*",(req,res)=>{
+imageRouter.get("/uploads/certificateModuleImages/*",(req,res)=>{
     try {
-        let p = req.url;
-        let i= 0;
-        while(p[0]=='/' || p[0]=='\\')p = p.substring(1,p.length);
-        // const image = path.join(__dirname,"../../../../",p)
-        const image = p;
-        console.log("Image path: ",p);
+        const p = req.url
+        const image = path.join(__dirname,"../../../../",p)
         const parts = image.split(".")
         if(parts[parts.length-1].toLocaleLowerCase()=="png" || parts[parts.length-1].toLocaleLowerCase()=="jpg" || parts[parts.length-1].toLocaleLowerCase()=="jpeg"){return res.sendFile(image)}
         else{
